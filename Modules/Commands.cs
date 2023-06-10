d5e63220e9ab129150a9f6add43603ad2cdd00ef
guledm12 <guledmo12@gmail.com>
2023-06-10 14:45:09 -0400

    Added commands file

---
+++ Modules/Commands.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace TutorialBot.Modules{
+    public class Commands : ModuleBase<SocketCommandContext>{
+        [Command("ping")]
+        public async Task Ping(){
+            await ReplyAsync("pong");
+        }
+
+    }
+}
